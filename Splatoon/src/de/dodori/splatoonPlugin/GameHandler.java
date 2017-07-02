edc37c59c3dc11f8fa190f1e436291f301d639b4
TestingDC <eseth3@yahoo.com>
2017-07-02 00:41:58 -0500

    Added gamehnadler stuff

--- Splatoon/src/de/dodori/splatoonPlugin/GameHandler.java
+++ Splatoon/src/de/dodori/splatoonPlugin/GameHandler.java
@@ -1,8 +1,39 @@
 package de.dodori.splatoonPlugin;
 
+import java.util.ArrayList;
+
+import org.bukkit.DyeColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
-public class GameHandler
-  implements Listener
-{
+public class GameHandler implements Listener {
+
+        ArrayList<PlayerGameData> teamOne = new ArrayList<PlayerGameData>();
+        ArrayList<PlayerGameData> teamTwo = new ArrayList<PlayerGameData>();
+        int teamLimit = 2;
+
+    public void joinGame(Player player) {
+        if (Math.random() > 0.5) {
+            PlayerGameData tempPlayerData = new PlayerGameData(player, true);
+            if (teamTwo.size() < teamLimit)
+                teamOne.add(tempPlayerData);
+        } else {
+            PlayerGameData tempPlayerData = new PlayerGameData(player, false);
+            if (teamTwo.size() < teamLimit)
+                teamTwo.add(tempPlayerData);
+        }
+    }
+}
+
+class PlayerGameData {
+    Player player;
+    int blocksInked;
+    // int deaths;
+    // int kills;
+    boolean isTeamOne;
+
+    public PlayerGameData(Player player, boolean isTeamOne) {
+        this.player = player;
+        this.isTeamOne = isTeamOne;
+    }
 }
